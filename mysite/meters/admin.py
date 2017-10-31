0a87ffa783af158a85ef262c45b0449d35e0cb15
batcomputer <batcomputerinfo@gmail.com>
2017-10-31 18:18:34 +0530

    Update admin.py

--- mysite/meters/admin.py
+++ mysite/meters/admin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import EnergyModel
+from .models import SITModel, BlockIVModel
 from django.contrib import admin
 
-admin.site.register(EnergyModel)
+admin.site.register(SITModel)
+admin.site.register(BlockIVModel)
 # Register your models here.
