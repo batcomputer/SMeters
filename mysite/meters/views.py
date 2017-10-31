3acd230ca4dd0eee92955a1e07b5a35454d20c88
batcomputer <batcomputerinfo@gmail.com>
2017-10-31 18:22:32 +0530

    Update views.py

--- mysite/meters/views.py
+++ mysite/meters/views.py
@@ -2,17 +2,23 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import EnergyModel
+from .models import SITModel, BlockIVModel,UdaigiriModel
 from django.core import serializers
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    data= EnergyModel.objects.all()
-   
+    data= SITModel.objects.all()
+    
     return render(request,'meters/index.html',{'energykwh': data})
 
 def jason(request):
-    objs = EnergyModel.objects.all()
-    jsondata = serializers.serialize('json',objs, fields=('energy'))#,'DateandTime'))
+    objs = SITModel.objects.all()
+    jsondata = serializers.serialize('json',objs, fields=('the_energy','the_date'))
+    return HttpResponse(jsondata, content_type='application/json')
+
+
+def hostel(request):
+    objs = BlockIVModel.objects.all()
+    jsondata = serializers.serialize('json',objs, fields=('the_energydata','the_time'))
     return HttpResponse(jsondata, content_type='application/json')
